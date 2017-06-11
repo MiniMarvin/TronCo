7fc46c34d5ea728019361255228a06dcb59193b6
Michael Barney <gatomaiquinho@gmail.com>
2017-06-11 23:10:19 +0000

    Configurando os arquivos para manter up-to-date com o repositório na nuvem.

--- menu.c
+++ menu.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "lib/client.h"
 
+#define size 40
 
 void limpar_tela();
 
 int menu();
 int jogo();
-
+void delay(unsigned int mseconds);
 
 int main(){
     int running = 1;
     int estado = 0; //0 = menu //1 = jogar
-    //int cursor = 0; //posição do cursor
-    
+
     while (running == 1){
         printf("ESTADO = %d\n", estado);
         switch(estado){
@@ -34,14 +35,11 @@ int main(){
     }
 }
 
-void limpar_tela(){
-    printf("\e[1;1H\e[2J");
-}
-
 int menu(){
         int cursor = 0; 
-        
-        while(1){ //eternamente
+        char read;
+        //Executa enquanto o usuário escolher
+        while(1){ 
             printf("\n\n");
             if(cursor == 0) printf("->");
             printf("            JOGAR\n\n");
@@ -50,7 +48,7 @@ int menu(){
             if(cursor == 2) printf("->");
             printf("            QUIT\n\n");
             
-            char read;
+            
             read = getch();
             //printf("EU RECEBI: %c", read);
             switch(read){
@@ -69,15 +67,99 @@ int menu(){
                 case ' ':
                     return cursor + 1;//retorna o estado
             }
-            
-            
-            
             limpar_tela();
         }
 }
 
-int  jogo(){
-    printf("O JOGO\n");
+int jogo(){
+    int myX = 0;
+    int myY = 0;
+    
+    int running = 1;
+    
+    int dir = 0; //0 = direita; 1 = baixo; 2 = esquerda; 3 = cima 
+    int tabuleiro [size][size];
+    
+    //preencher matriz
+    int i = 0;
+    for(i; i < size; i++){
+        int j = 0;
+        for(j; j < size; j++){
+            tabuleiro[i][j] = ' ';
+        }
+    }
+    
+    while(running == 1){
+        tabuleiro[myX][myY] = 'X';
+        
+        //imprimir matriz
+        int i = 0;
+        for(i; i < size; i++){
+            int j = 0;
+            for(j; j < size; j++){
+                printf("%c ", tabuleiro[j][i]);
+            }
+            printf("\n");
+        }    
+        
+        //atualizar meu X e Y
+        switch(dir){
+            case 0:
+                myX++;
+                break;
+                
+            case 1:
+                myY++;
+                break;
+            
+            case 2:
+                myX--;
+                break;
+            
+            case 3:
+                myY--;
+                break;
+        }
+        
+        
+        //ler os valor
+        char read;
+        read = getch();
+        //printf("EU RECEBI: %c", read);
+        switch(read){
+            case 's':
+                    dir = 1;
+                    break;
+            case 'a':
+                    dir = 2;
+                    break;
+                    
+            case 'w':
+                    dir = 3;
+                    break;
+            
+            case 'd':
+                    dir = 0;
+                    break;
+                    
+        }
+        
+        //dar um delay
+        delay(10000);   
+        
+        //limpar tela
+        limpar_tela();
+    }
+    
     return 0;
 }
 
+//ferramentas
+void limpar_tela(){
+    printf("\e[1;1H\e[2J");
+}
+
+void delay(unsigned int mseconds){
+    clock_t goal = mseconds + clock();
+    while (goal > clock());
+}
