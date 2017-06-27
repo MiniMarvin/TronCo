92fe7b46f0d7a779728aaf0c90f36621a09b5a6f
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2017-06-27 10:15:59 +0000

    Atualização para que o cliente possa ser desconectado e voltar com a mesma ID

---
+++ v0.08(Id Desconecta)/libTronco/tronco.h
@@ -0,0 +1,33 @@
+#ifndef __TRONCO_GAME_H__
+#define __TRONCO_GAME_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+#define SIZEX 25
+#define SIZEY 45
+#define WIDTH 1550
+#define HEIGTH 870
+#define MAXCLIENTS 4 //Se ligar na biblioteca deles
+
+typedef struct{
+  int dir;
+}clientMsg;
+
+typedef struct{
+  char matriz[SIZEX][SIZEY];
+  int dir[MAXCLIENTS];
+  int statusPlayer;
+  // int status; Inteiro para dizer se o cliente esta vivo no jogo
+}serverMsg;
+
+/**
+ * @brief      Espera um tempo em mili segundos.
+ *
+ * @param[in]  mseconds  O tempo a esperar.
+ */
+void delay(unsigned int mseconds);
+
+#endif
